b3593626bb10480743273ff5a319a1dd26b6e034
Omar Mohammed Elsayed Elbedawy <omar.1824039@stemksheikh.moe.edu.eg>
2025-09-11 20:56:02 +0000

    Can u make everything in the web app follow these, for sire u can change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </head>
